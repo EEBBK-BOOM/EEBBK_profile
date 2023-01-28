e7ae4e8f6fac484c7a7ae7d44bd1c6a4c3e5c64e
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-01-29 02:11:18 +0800

    Add files via upload

---
+++ non-root_profile.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+set apk-1=https://download.eebbk.top/eebbk/2023/01/24/iNrhUGxnOB4TLIH5_apkinstaller.apk
+set apk-2=https://download.eebbk.top/eebbk/2023/01/24/XkXBwYpNagZCqFjR_shizuku.apk
+set apk-3=https://download.eebbk.top/eebbk/2023/01/24/gWLN9wrGJRsZt70k_shortcutcreator.apk
+set apk-4=https://download.eebbk.top/eebbk/2023/01/24/Bx493PmUr8LVxhDW_xbrowser.apk
+
+set apk_name-1=apkinstaller.apk
+set apk_name-2=shizuku.apk
+set apk_name-3=shortcutcreator.apk
+set apk_name-4=xbrowser.apk

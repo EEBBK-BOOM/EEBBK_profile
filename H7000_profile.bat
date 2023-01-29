cd24e273a9d6bf3a7dba88036ae6dd4213213f9c
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-01-30 03:42:11 +0800

    Update H7000_profile.bat

--- H7000_profile.bat
+++ H7000_profile.bat
@@ -4,3 +4,4 @@ set twrp=https://download.eebbk.top/eebbk/2023/01/30/tbfZBDpDRijGu32f_twrp-3.5.2
 set msic_stock=https://download.eebbk.top/eebbk/2023/01/30/1C9IYVrbe6wcUCdZ_misc_stock.bin
 set msic_rec=https://download.eebbk.top/eebbk/2023/01/30/Ky3maI9wSPkpCyG6_misc_recovery.bin
 set prog=https://download.eebbk.top/eebbk/2023/01/30/YwBPzieh716VdqVv_prog_emmc_firehose_Sdm660_ddr.elf
+set elf=prog_emmc_firehose_Sdm660_ddr.elf

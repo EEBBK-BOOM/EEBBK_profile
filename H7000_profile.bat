0f9ab6516560387abddc43124619e0dccdb878b0
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-01-30 03:32:57 +0800

    Add files via upload

---
+++ H7000_profile.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+set stock_boot=https://download.eebbk.top/eebbk/2023/01/30/PMtDeUpE6SRuDlW5_boot.img
+set twrp=https://download.eebbk.top/eebbk/2023/01/30/tbfZBDpDRijGu32f_twrp-3.5.2_9-0-H7000.img
+set msic_stock=https://download.eebbk.top/eebbk/2023/01/30/1C9IYVrbe6wcUCdZ_misc_stock.bin
+set msic_rec=https://download.eebbk.top/eebbk/2023/01/30/Ky3maI9wSPkpCyG6_misc_recovery.bin

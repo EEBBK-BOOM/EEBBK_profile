f316d005816a4566034804370a1c811d2b881ec2
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-02-04 03:13:17 +0800

    Create H3000X_profile.bat

---
+++ H3000X_profile.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+set stock_boot=https://download.eebbk.top/eebbk/2023/01/30/PMtDeUpE6SRuDlW5_boot.img
+set twrp=https://download.eebbk.top/eebbk/2023/01/30/tbfZBDpDRijGu32f_twrp-3.5.2_9-0-H7000.img

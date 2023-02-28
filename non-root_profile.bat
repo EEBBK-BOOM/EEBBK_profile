2b05366645c790e9b8e23ea27dc9fde81374c5a2
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-02-28 23:03:55 +0800

    Update non-root_profile.bat

--- non-root_profile.bat
+++ non-root_profile.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 set apk-1=https://download.eebbk.top/eebbk/2023/01/24/iNrhUGxnOB4TLIH5_apkinstaller.apk
-set apk-2=https://download.eebbk.top/eebbk/2023/02/04/DITK62vI4TdZKRHI_shizuku.apk
+set apk-2=https://download.eebbk.top/eebbk/2023/02/28/Eg24MGSu9Axe9Lv9_shizuku.apk
 set apk-3=https://download.eebbk.top/eebbk/2023/01/24/gWLN9wrGJRsZt70k_shortcutcreator.apk
-set apk-4=https://download.eebbk.top/eebbk/2023/01/24/Bx493PmUr8LVxhDW_xbrowser.apk
+set apk-4=https://download.eebbk.top/eebbk/2023/02/28/zzvR60uW4wQ0htze_xbrowser.apk
 
 set apk_name-1=apkinstaller.apk
 set apk_name-2=shizuku.apk

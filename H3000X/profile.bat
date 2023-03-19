75ad9d399cae5395533fc76665b526d832abb5c8
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-03-20 01:03:45 +0800

    Update profile.bat

--- H3000X/profile.bat
+++ H3000X/profile.bat
@@ -2,6 +2,6 @@
 set link=https://download.eebbk.top/eebbk/
 set stock_boot=%link%2023/03/20/bTtReVYb8vmrPHfX_boot.7z
 set twrp=%link%2023/03/20/oapDLAlDN3TGScRn_twrp.7z
-set git=https://raw.kgithub.com/
+set mtkclient=https://codeload.kgithub.com/bkerler/mtkclient/zip/refs/heads/main
 set checksum_twrp=22B8C3D8
 set checksum_boot=AB364E8D

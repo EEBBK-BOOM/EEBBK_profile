86e898d9cadfd7cc66d76e26084779e67c4fb0ec
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-03-19 15:32:09 +0800

    Update non-root_profile.bat

--- non-root_profile.bat
+++ non-root_profile.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
-set apk-1=https://download.eebbk.top/eebbk/2023/01/24/iNrhUGxnOB4TLIH5_apkinstaller.apk
-set apk-2=https://download.eebbk.top/eebbk/2023/02/28/Eg24MGSu9Axe9Lv9_shizuku.apk
-set apk-3=https://download.eebbk.top/eebbk/2023/01/24/gWLN9wrGJRsZt70k_shortcutcreator.apk
-set apk-4=https://download.eebbk.top/eebbk/2023/02/28/zzvR60uW4wQ0htze_xbrowser.apk
+set apk-1=https://download.eebbk.top/eebbk/2023/03/06/ZmUph6BmkLTFIY0I_r-packageinstalles_2.6.2-beta(286).apk
+set apk-2=https://download.eebbk.top/eebbk/2023/03/17/jO3aeZK0GOyNBXcW_Shizuku-13.2.1.r979.apk
+set apk-3=https://download.eebbk.top/eebbk/2023/03/16/jci7x1GpiBQ0urtm_shortcutcreator.apk
+set apk-4=https://download.eebbk.top/eebbk/2023/03/16/reHzajqFpEKVPqPW_xbrowser_4.0.1(706).apk
 
-set apk_name-1=apkinstaller.apk
+set apk_name-1=r-packageinstalles.apk
 set apk_name-2=shizuku.apk
 set apk_name-3=shortcutcreator.apk
 set apk_name-4=xbrowser.apk

0d3cafb8898eda25e959ca047eb9da2e870c5153
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-03-21 01:07:24 +0800

    Create profile.bat

---
+++ H130/profile.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+set link=https://download.eebbk.top/eebbk/
+set stock_boot=%link%2023/03/21/AcvnzsI1lTOtKQPJ_boot_stock.7z
+set magisk=%link%2023/03/21/ld70qZDvFf7MRbQx_boot_magisk.7z
+set checksum_magisk=182A7EE6
+set checksum_boot=95A2A579

7ce2a484a12c6a95bf799885a5c67b6e0ff329c8
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-03-21 01:44:18 +0800

    Update profile.bat

--- H130/profile.bat
+++ H130/profile.bat
@@ -5,4 +5,4 @@ set link=https://download.eebbk.top/eebbk/
 set stock_boot=%link%2023/03/21/AcvnzsI1lTOtKQPJ_boot_stock.7z
 set magisk_boot=%link%2023/03/21/ld70qZDvFf7MRbQx_boot_magisk.7z
 set checksum_magisk=182A7EE6
-set checksum_boot=95A2A579
+set checksum_stock=95A2A579

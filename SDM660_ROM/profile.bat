ba42d7fb0c3350073e8710991f5ca9b4ae86819b
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-04-15 22:30:02 +0800

    Create profile.bat

---
+++ SDM660_ROM/profile.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+set link=https://download.eebbk.top/eebbk/
+set H7000_ver=V1.4.3
+set H110_ver=V1.3.0
+set H7000_rom=44C2259E
+set H7000_firmware=8A2920BE
+set H110_rom=AAD9E405
+set H110_firmware=EEEDCB7E

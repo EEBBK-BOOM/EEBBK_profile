3d99204feb0dbb99fb671d086f7382cba2d7bf39
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-07-19 02:08:14 +0800

    Update profile_V11.bat

--- H7000/profile_V11.bat
+++ H7000/profile_V11.bat
@@ -2,7 +2,7 @@
 set link=https://ycly.lanzouw.com/
 set stock_boot=%link%iIgnN12vgf1c
 set twrp=%link%iNulR12sjsuj
-set msic=%link%iZEI412vgete
+set misc=%link%ipxko12vghad
 set elf=prog_emmc_firehose_Sdm660_ddr.elf
 set checksum_twrp=4ABC74EF
 set checksum_boot=4DA4D231

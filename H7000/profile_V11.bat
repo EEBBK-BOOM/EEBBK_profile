e3f9aacb464155905865af07c0c5664c4306fdf2
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-07-23 03:23:36 +0800

    Update profile_V11.bat

--- H7000/profile_V11.bat
+++ H7000/profile_V11.bat
@@ -6,6 +6,7 @@ set misc=%link%ipxko12vghad
 set toolbox=%link%ivt3r138wyji
 set magisk=%link%iGboN0xh8r7i
 set elf=prog_emmc_firehose_Sdm660_ddr.elf
+set twrp_file=twrp-3.7.0_9-20230403-H7000.img
 set lzdl=https://eebbk-boom.github.io/EEBBK_profile/bin/lzdl.7z
 set checksum_twrp=4ABC74EF
 set checksum_boot=4DA4D231

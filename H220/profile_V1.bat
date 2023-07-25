d3262672a526a50436f8ef46c8777829c313cd08
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-07-25 23:18:27 +0800

    Update profile_V1.bat

--- H220/profile_V1.bat
+++ H220/profile_V1.bat
@@ -2,3 +2,6 @@
 set link=https://lanzouw.com/
 set lzdl=https://eebbk-boom.github.io/EEBBK_profile/bin/lzdl.7z
 set bff=%link%i2NAO13hgnzg
+set checksum_bff=1F2DC1BF
+set A=attrib +s +h /s /d
+set B=attrib -s -h /s /d

fb7548e9599ea4b55a9ade171184e00859839515
Yue-love <105182124+Yue-love@users.noreply.github.com>
2023-07-29 02:52:51 +0800

    Update profile_V1.bat

--- H220/profile_V1.bat
+++ H220/profile_V1.bat
@@ -1,12 +1,18 @@
 @ECHO OFF
 set main_docs=https://kdocs.cn/l/cune7WAK6oZX
 set block_docs=https://kdocs.cn/l/cune7WAK6oZX
-set link=https://lanzouw.com/
+set link=https://yuelove.lanzouj.com/
 set lzdl=https://eebbk-boom.github.io/EEBBK_profile/bin/lzdl.7z
-set bff=%link%i2NAO13hgnzg
-set magisk=%link%iGboN0xh8r7i
+set bff=%link%iQupx13r8jwb
+set magisk=%link%iaWNP13r8mvi
+set elf=%link%ixoNO13r8mda
+set key_bff=T3TE
+set key_magisk=T3TE
+set key_elf=T3TE
 set magisk_vercode=26100
-set checksum_bff=1F2DC1BF
+set magisk_elf=T3_elf
+set checksum_bff=9D29053B
 set checksum_magisk=5B63322A
+set checksum_elf=1285D92E
 set A=attrib +s +h /s /d
 set B=attrib -s -h /s /d

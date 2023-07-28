e50bc8e1010186ca507791355187ecd693f6ecbc
Yue-love <105182124+Yue-love@users.noreply.github.com>
2023-07-29 05:56:17 +0800

    Update profile_V1.bat

--- H220/profile_V1.bat
+++ H220/profile_V1.bat
@@ -2,11 +2,12 @@
 set main_docs=https://kdocs.cn/l/cune7WAK6oZX
 set block_docs=https://kdocs.cn/l/cune7WAK6oZX
 set link=https://yuelove.lanzouj.com/
+set link2=https://lanzoui.com/
 set lzdl=https://eebbk-boom.github.io/EEBBK_profile/bin/lzdl.7z
 set bff=%link%iQr1S13r9pbc
 set magisk=%link%iaWNP13r8mvi
 set elf=%link%ixoNO13r8mda
-set toolbox=ivt3r138wyji
+set toolbox=%link2%ivt3r138wyji
 set key_bff=T3TE
 set key_magisk=T3TE
 set key_elf=T3TE

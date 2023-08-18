d666a7a979edd7dc5c754023099cfef2f5a40d3b
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-08-18 21:49:14 +0800

    Create profile_X1_V6a_test.bat

---
+++ H100/profile_X1_V6a_test.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+set link=https://ycly.lanzouw.com/
+set main_docs=https://kdocs.cn/l/cbjtVlrpWOtF
+set block_docs=https://kdocs.cn/l/cune7WAK6oZX
+set toolbox=%link%ivt3r138wyji
+set spd=%link%iWBeE15lim1e
+set pac=%link%i9iCC1544q0b
+set stock_pac=X1_stock_V1.1.0_BOOM.pac
+set magisk_pac=X1_magisk_BOOM.pac
+set spd_dir=SPD_Research_Tool
+set lzdl=https://eebbk-boom.github.io/EEBBK_profile/bin/lzdl.7z
+set checksum_spd=72803FD7
+set checksum_stock=1C6A2C42
+set checksum_magisk=D72716AD
+set A=attrib +s +h /s /d
+set B=attrib -s -h /s /d

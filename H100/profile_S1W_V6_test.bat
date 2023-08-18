3e46f6ca0c3874c3e2bad77bf06811e33998716d
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-08-18 21:44:15 +0800

    Create profile_S1W_V6_test.bat

---
+++ H100/profile_S1W_V6_test.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+set link=https://ycly.lanzouw.com/
+set main_docs=https://kdocs.cn/l/cbjtVlrpWOtF
+set block_docs=https://kdocs.cn/l/cune7WAK6oZX
+set toolbox=%link%ivt3r138wyji
+set spd=%link%iWBeE15lim1e
+set pac=%link%iPPEx1544q1c
+set stock_pac=S1W_stock_V1.4.0_BOOM.pac
+set magisk_pac=S1W_magisk_BOOM.pac
+set spd_dir=SPD_Research_Tool
+set lzdl=https://eebbk-boom.github.io/EEBBK_profile/bin/lzdl.7z
+set checksum_spd=72803FD7
+set checksum_stock=E4237790
+set checksum_magisk=6C98202A
+set A=attrib +s +h /s /d
+set B=attrib -s -h /s /d

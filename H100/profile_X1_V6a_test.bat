d6d9e16eedd87de5b9e388509460463db592a0a7
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-08-18 22:17:02 +0800

    Update profile_X1_V6a_test.bat

--- H100/profile_X1_V6a_test.bat
+++ H100/profile_X1_V6a_test.bat
@@ -5,6 +5,7 @@ set block_docs=https://kdocs.cn/l/cune7WAK6oZX
 set toolbox=%link%ivt3r138wyji
 set spd=%link%iWBeE15lim1e
 set pac=%link%i9iCC1544q0b
+set folder=%link%iln5S15nlosd
 set stock_pac=X1_stock_V1.1.0_BOOM.pac
 set magisk_pac=X1_magisk_BOOM.pac
 set spd_dir=SPD_Research_Tool

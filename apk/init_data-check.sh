0aab8e265be9932d47ee328cec72166c29766217
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-08-19 21:27:45 +0800

    Update init_data-check.sh

--- apk/init_data-check.sh
+++ apk/init_data-check.sh
@@ -11,12 +11,12 @@ case "$1" in
 com.ycly.EEBBK.toolbox)
 apk='com.ycly.EEBBK.toolbox'
 name='EEBBK Toolbox'
-version='2.0.2'
-versionCode=202
-author='EEBBK BOOM'
-description='更新日志：更换下载通道'
-time='2023年5月28号'
-    [[ $Choice = 1 ]] && Download -url 'https://kgithub.com/EEBBK-BOOM/EEBBK_profile/releases/download/Toolbox/Toolbox_2.0.2.apk' "$1.apk" '16545099' '8befc98c310febf9521e482492a68d05' "$1.apk"
+version='2.0.3'
+versionCode=203
+author='EEBBK BOOM团队'
+description='更新日志：修复满速下载通道'
+time='2023年8月19号'
+    [[ $Choice = 1 ]] && Download -url 'https://kgithub.com/EEBBK-BOOM/EEBBK_profile/releases/download/Toolbox/Toolbox.apk' "$1.apk" '17572837' 'ff03fc16aa537969f6346b2bd35fe7b7' "$1.apk"
 ;;
 
 exit_sideload)

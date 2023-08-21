cbdc30a132e91e93484b759def71193b87af08af
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-08-22 01:51:11 +0800

    Create profile_V6_test.bat

---
+++ H3000X/profile_V6_test.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+set link=https://ycly.lanzouw.com/
+set main_docs=https://kdocs.cn/l/cbjtVlrpWOtF
+set block_docs=https://kdocs.cn/l/cune7WAK6oZX
+set stock_boot=%link%i7w91161r5zg
+set twrp=%link%iJRdg161r5vc
+set mtkclient=%link%irMur15xwc2b
+set toolbox=%link%itvaS15uxthg
+set magisk=%link%iGboN0xh8r7i
+set adb=%link%i0nal13buumd
+set twrp_file=twrp-3.7.0_11-20230405-H3000X.img
+set mtkclient_dir=mtkclient-main
+set lzdl=https://eebbk-boom.github.io/EEBBK_profile/bin/lzdl.7z
+set checksum_twrp=A1D720F4
+set checksum_boot=AB364E8D
+set checksum_magisk=5B63322A
+set checksum_adb=6DF5B994
+set A=attrib +s +h /s /d
+set B=attrib -s -h /s /d

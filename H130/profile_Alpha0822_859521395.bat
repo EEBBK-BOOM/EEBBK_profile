c472948768522f50125f5b5353aa1f8cd809d87f
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-08-22 23:19:36 +0800

    Create profile_Alpha0822_859521395.bat

---
+++ H130/profile_Alpha0822_859521395.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+set link=https://ycly.lanzouw.com/
+set main_docs=https://kdocs.cn/l/cbjtVlrpWOtF
+set block_docs=https://kdocs.cn/l/cune7WAK6oZX
+set stock_boot=%link%iIgnN12vgf1c
+set twrp=%link%iNulR12sjsuj
+set toolbox=%link%itvaS15uxthg
+set magisk=%link%iGboN0xh8r7i
+set adb=%link%i0nal13buumd
+set twrp_file=twrp-3.7.0_11-20230822-H130.img
+set lzdl=https://eebbk-boom.github.io/EEBBK_profile/bin/lzdl.7z
+set checksum_twrp=4ABC74EF
+set checksum_boot=95A2A579
+set checksum_magisk=5B63322A
+set checksum_adb=6DF5B994
+set A=attrib +s +h /s /d
+set B=attrib -s -h /s /d

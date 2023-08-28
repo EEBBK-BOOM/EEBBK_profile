316d33fee3dfdafc43284527cc2388391765fdac
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2023-08-28 21:41:27 +0800

    Update and rename profile.bat to profile_Alpha0823_859521395test2.bat

--- H130/profile.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-set magisk_image=boot_magisk.img
-set stock_image=boot_stock.img
-set link=https://raw.kgithub.com/EEBBK-BOOM/EEBBK_profile/main/H130/
-set stock_boot=%link%boot_stock.7z
-set magisk_boot=%link%boot_magisk.7z
-set checksum_magisk=182A7EE6
-set checksum_stock=95A2A579

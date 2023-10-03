4ed644309ea198dc0a12583d2889b9c9c2ba4f2a
Yue-love <105182124+Yue-love@users.noreply.github.com>
2023-10-03 21:08:40 +0800

    Update profile_V1_SM6150S7.bat

--- H210/profile_V1_SM6150S7.bat
+++ H210/profile_V1_SM6150S7.bat
@@ -1,1 +1,20 @@
-
+set main_docs=https://kdocs.cn/l/cune7WAK6oZX
+set block_docs=https://kdocs.cn/l/cune7WAK6oZX
+set link=https://yuelove.lanzouj.com/
+set lzdl=https://down.eebbk.top/bin/lzdl.7z
+set bff=%link%i72pU18ooe1i
+set magisk=%link%iJyrv15wcq8f
+set elf=%link%ixoNO13r8mda
+set toolbox=%link%iX1Fa17qt5sh
+set driver=%link%iEPJm15796ti
+set update_log=%link2%iCYLV18ofihe
+set key_bff=T3TE
+set key_magisk=T3TE
+set key_elf=T3TE
+set key_driver=T3TE
+set magisk_vercode=25200
+set checksum_bff=C892F69C
+set checksum_magisk=3A965DEE
+set checksum_elf=1285D92E
+set A=attrib +s +h /s /d
+set B=attrib -s -h /s /d

2d8f9b0cad7774e0a8fd8dbe4f0bbe5d11cbfec4
Yue-love <105182124+Yue-love@users.noreply.github.com>
2024-02-15 16:40:29 +0800

    Update profile_V1_SDM660.bat

--- H110/profile_V1_SDM660.bat
+++ H110/profile_V1_SDM660.bat
@@ -3,27 +3,34 @@ set main_docs=https://kdocs.cn/l/cune7WAK6oZX
 set block_docs=https://kdocs.cn/l/cune7WAK6oZX
 set link=https://yuelove.lanzn.com/
 set lzdl=https://down.eebbk.top/bin/lzdl.7z
+
 set bff=%link%ilyjx1mmo8le
 set Apatch=%link%inFs31mmjt0j
 set elf=%link%ivkt61moefob
 set toolbox=%link%iX1Fa17qt5sh
 set driver=%link%iEPJm15796ti
-set boot=%link%i4ho51moegsb
+set apboot=%link%i4ho51moegsb
+set boot=
 set update_log=%link%iXBcH1mi1uzi
 set logo=%link%iK54c1anp6ub
+
 set keyR=EEBBK-BOOM
 set device=H110
+
 set key_bff=S5PTE
 set key_Apatch=S5PTE
 set key_elf=S5PTE
 set key_boot=S5PTE
 set key_driver=1234
+
 set Apatch_vercode=apatch
+
 set checksum_bff=BEB75531
 set checksum_Apatch=7138C29B
 set checksum_elf=8D2631AE
 set checksum_rom=11A932E0
 set checksum_firmware=3B304279
 set checksum_boot=F2193288
+
 set A=attrib +s +h /s /d
 set B=attrib -s -h /s /d

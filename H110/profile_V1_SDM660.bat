70276ea0bce7ccf5b4b07ae7c9ab5bc5651ea52d
Yue-love <105182124+Yue-love@users.noreply.github.com>
2024-02-15 17:06:31 +0800

    Update profile_V1_SDM660.bat

--- H110/profile_V1_SDM660.bat
+++ H110/profile_V1_SDM660.bat
@@ -9,8 +9,8 @@ set Apatch=%link%inFs31mmjt0j
 set elf=%link%ivkt61moefob
 set toolbox=%link%iX1Fa17qt5sh
 set driver=%link%iEPJm15796ti
-set apboot=%link%i4ho51moegsb
-set boot=
+set apboot=%link%i808H1obvqwb
+set boot=id0yi1obvupi
 set update_log=%link%iXBcH1mi1uzi
 set logo=%link%iK54c1anp6ub
 
@@ -18,8 +18,9 @@ set keyR=EEBBK-BOOM
 set device=H110
 
 set key_bff=S5PTE
-set key_Apatch=S5PTE
+set key_APK=S5PTE
 set key_elf=S5PTE
+set key_apboot=S5PTE
 set key_boot=S5PTE
 set key_driver=1234
 
@@ -30,7 +31,9 @@ set checksum_Apatch=7138C29B
 set checksum_elf=8D2631AE
 set checksum_rom=11A932E0
 set checksum_firmware=3B304279
-set checksum_boot=F2193288
+set checksum_apboot=5C5EDD38
+set checksum_boot=386DF4D7
+set check_toolbox=E1432F52
 
 set A=attrib +s +h /s /d
 set B=attrib -s -h /s /d

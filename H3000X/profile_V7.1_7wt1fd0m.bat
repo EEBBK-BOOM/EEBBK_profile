ec91f076269b4fd250ca6dd168731739df55993a
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2024-06-16 20:10:31 +0800

    Update and rename profile_V6.3_7wt1fd0m.bat to profile_V7.1_7wt1fd0m.bat

--- H3000X/profile_V7.1_7wt1fd0m.bat
+++ H3000X/profile_V7.1_7wt1fd0m.bat
@@ -1,13 +1,13 @@
 @ECHO OFF
-set link=https://ycly.lanzouw.com/
+set link=https://ycly.lanzn.com/
 set main_docs=https://kdocs.cn/l/cdoB9VJ9XApf
 set block_docs=https://kdocs.cn/l/cune7WAK6oZX
 set stock_boot=%link%i7w91161r5zg
 set twrp=%link%iEkgB16w0e5e
 set mtkclient=%link%iMifi1a5esod
 set python=%link%iOc52161qx6j
-set toolbox=%link%iX1Fa17qt5sh
-set magisk=%link%itAp917ok08b
+set toolbox=%link%i58ET21zzy4h
+set magisk=%link%iuhRL1nrxlta
 set adb=%link%i0nal13buumd
 set twrp_file=twrp-3.7.0_11-20230828-H3000X.img
 set mtkclient_dir=mtkclient-main
@@ -15,7 +15,7 @@ set pypi=https://repo.huaweicloud.com/repository/pypi/simple/
 set lzdl=https://down.eebbk.top/bin/lzdl.7z
 set checksum_twrp=39826DB9
 set checksum_boot=AB364E8D
-set checksum_magisk=05304293
+set checksum_magisk=36E02F59
 set checksum_adb=6DF5B994
 set checksum_mtkclient=A595DD04
 set A=attrib +s +h /s /d

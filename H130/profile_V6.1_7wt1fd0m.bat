3c4fa0754b3d6c49942ac06e60aeb055f44388fa
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2024-06-16 20:05:27 +0800

    Update and rename profile_V5.2_7wt1fd0m.bat to profile_V6.1_7wt1fd0m.bat

--- H130/profile_V6.1_7wt1fd0m.bat
+++ H130/profile_V6.1_7wt1fd0m.bat
@@ -1,14 +1,15 @@
 @ECHO OFF
-set link=https://ycly.lanzouw.com/
+set link=https://ycly.lanzn.com/
 set main_docs=https://kdocs.cn/l/cn3ogq8gQJFy
 set block_docs=https://kdocs.cn/l/cune7WAK6oZX
 set stock_boot=%link%iW7YX164oqkh
 set twrp=%link%in4Wq16w0g2d
-set toolbox=%link%iX1Fa17qt5sh
+set toolbox=%link%i58ET21zzy4h
 set magisk=%link%itAp917ok08b
 set adb=%link%i0nal13buumd
 set twrp_file=twrp-3.7.0_11-20230829-H130.img
 set lzdl=https://down.eebbk.top/bin/lzdl.7z
+set checksum_toolbox=B2C7FD5A
 set checksum_twrp=C428D586
 set checksum_boot=95A2A579
 set checksum_magisk=05304293
